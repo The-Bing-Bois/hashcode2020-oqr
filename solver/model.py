9a685c85707dd1bbedcfd127971c551469bab4f6
Mariano <marianosciacco@gmail.com>
2020-02-20 20:33:32 +0100

    Save method with parameters

--- solver/model.py
+++ solver/model.py
@@ -2,35 +2,6 @@ from pathlib import Path
 import attr
 
 
-@attr.s(frozen=True, slots=True, auto_attribs=True)
-class InputReader:
-    filename: str = attr.ib(converter=str)
-
-    def parse(self) -> None:
-        content = Path(self.filename).read_text().split("\n")
-
-        books_number, libraries_number, days = (int(x) for x in content[0].split(" "))
-
-        book_scores = [int(x) for x in content[1].split(" ")]
-
-        assert len(book_scores) == books_number
-
-        print(books_number, libraries_number, days)
-        print(book_scores)
-
-        raise NotImplementedError()
-
-
-@attr.s(frozen=True, slots=True, auto_attribs=True)
-class OutputWriter:
-    filename: str = attr.ib(converter=str)
-
-    def save(self) -> None:
-        outfile = Path("out").joinpath(self.filename)
-
-        outfile.write_text(...)
-
-
 class Book:
     def __init__(self, bid, score):
         self.bid = bid
@@ -68,3 +39,32 @@ class BooksCollection:
     def add_book(self, book: Book):
         self.books.append(book)
 
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class InputReader:
+    filename: str = attr.ib(converter=str)
+
+    def parse(self) -> None:
+        content = Path(self.filename).read_text().split("\n")
+
+        books_number, libraries_number, days = (int(x) for x in content[0].split(" "))
+
+        book_scores = [int(x) for x in content[1].split(" ")]
+
+        assert len(book_scores) == books_number
+
+        print(books_number, libraries_number, days)
+        print(book_scores)
+
+        raise NotImplementedError()
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class OutputWriter:
+    filename: str = attr.ib(converter=str)
+
+    def save(self, booksCollection: BooksCollection, libraryCollection: LibraryCollection) -> None:
+        outfile = Path("out").joinpath(self.filename)
+
+        # outfile.write_text(...) to write
