3118a5cce71822ee12b06ff724ccf14febad4d46
1dotd4 <>
2020-02-20 23:18:34 +0100

    indentation fuck

--- solver/solve.py
+++ solver/solve.py
@@ -32,48 +32,48 @@ def solveBestBook(
     openLibraries = 0  ## ++ on open
 
     with tqdm(total=totalDays, ascii=True) as pbar:
-    while totalDays > 0:
-        openLibrariesToFill = openLibraries
-        for b in booksToRestore:
-            booksCollection.removeBook(b)
-        for l in librariesToRestore:
-            l.resetCountBooksPerDay()
-        booksToRestore = []
-        i = 0
+        while totalDays > 0:
+            openLibrariesToFill = openLibraries
+            for b in booksToRestore:
+                booksCollection.removeBook(b)
+            for l in librariesToRestore:
+                l.resetCountBooksPerDay()
+            booksToRestore = []
+            i = 0
 
-        while openLibrariesToFill > 0 and i < len(booksCollection.books):
-            currentBook = booksCollection.books[i]
-            isThereOneOpen = False
-            for l in currentBook.libraries:
-                if l.signin == 0:
-                    isThereOneOpen = True
-                    librariesToRestore.append(l)
-                    booksToRestore.append(currentBook)
-                    if l.scanBook(currentBook) == 0:
-                        openLibrariesToFill -= 1
-                    break
-            if not isThereOneOpen:
-                if currentlyOpening == None:
-                    if len(currentBook.libraries) > 0:
-                        currentlyOpening = min(
-                            currentBook.libraries,
-                            key=lambda x: x.signin if x.signin > 0 else 100000,
-                        )
-            i += 1
+            while openLibrariesToFill > 0 and i < len(booksCollection.books):
+                currentBook = booksCollection.books[i]
+                isThereOneOpen = False
+                for l in currentBook.libraries:
+                    if l.signin == 0:
+                        isThereOneOpen = True
+                        librariesToRestore.append(l)
+                        booksToRestore.append(currentBook)
+                        if l.scanBook(currentBook) == 0:
+                            openLibrariesToFill -= 1
+                        break
+                if not isThereOneOpen:
+                    if currentlyOpening == None:
+                        if len(currentBook.libraries) > 0:
+                            currentlyOpening = min(
+                                currentBook.libraries,
+                                key=lambda x: x.signin if x.signin > 0 else 100000,
+                            )
+                i += 1
 
-        if currentlyOpening == None and i < len(booksCollection.books) and len(booksCollection.books) > 0 and len(booksCollection.books[i].libraries):
-            currentlyOpening = min(
-                booksCollection.books[i].libraries,
-                key=lambda x: x.signin if x.signin > 0 else 100000,
-            )
-        if currentlyOpening:
-            currentlyOpening.signin -= 1
-            if currentlyOpening.signin == 0:
-                librariesToReturn.append(currentlyOpening)
-                currentlyOpening = None
-                openLibraries += 1
+            if currentlyOpening == None and i < len(booksCollection.books) and len(booksCollection.books) > 0 and len(booksCollection.books[i].libraries):
+                currentlyOpening = min(
+                    booksCollection.books[i].libraries,
+                    key=lambda x: x.signin if x.signin > 0 else 100000,
+                )
+            if currentlyOpening:
+                currentlyOpening.signin -= 1
+                if currentlyOpening.signin == 0:
+                    librariesToReturn.append(currentlyOpening)
+                    currentlyOpening = None
+                    openLibraries += 1
 
-        totalDays -= 1
+            totalDays -= 1
             pbar.update()
 
     return librariesToReturn
