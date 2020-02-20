5b0458ba61a7fb486eed86df3f6eb18d0c41c325
1dotd4 <>
2020-02-20 21:27:51 +0100

    Prima prova del solver

--- solver/solve.py
+++ solver/solve.py
@@ -8,5 +8,49 @@ def main(filename: str) -> None:
     model.InputReader(filename).parse()
 
 
-def solve() -> None:
+def solveBestBook(model: Model??) -> None:
+    day = model.day
+    books = model.books // need a copy to do stuff
+    libraries = model.libraries // need a copy to do stuff
+    currentlyOpening = nil // to tack currentlyOpeningLibraries
+    booksToRestore = [] // to track currently added books in that fancy book[i]
+    openLibraries = 0 // ++ on open
+
+    while (day > 0):
+        openLibrariesToFill = openLibraries
+        for b in booksToRestore:
+            books.remove(b)
+        i = 0
+        while openLibrariesToFill > 0:
+            currentBook = books[i]
+            if l = any(currentBook.libraries, .isOpen):
+                if l.appendBook(currentBook): // ritorna true se ultimo libro aggiungibile nella libreria
+                    openLibrarisToFill -= 1
+                booksToRestore.append(currentBook)
+            else:
+                if !currentlyOpening:
+                    l = min(currentBook.libraries, .signupTime):
+                    l.isOpening = True
+                    currentlyOpening = l
+            i += 1
+
+        if !currentlyOpening:
+            l = any(books[i].libraries, !.isOpen)
+
+        if currentlyOpening:
+            if (l.tempoDiAttesa -= 1) == 0:
+                currentlyOpening = nil
+                openLibraries += 1
+
+        day -= 1
+
+    return libraries
+
+                
+
+            
+    
+
+def solveFastLibrary() -> None:
+    // opt2 FastLibraryFirst
     pass
